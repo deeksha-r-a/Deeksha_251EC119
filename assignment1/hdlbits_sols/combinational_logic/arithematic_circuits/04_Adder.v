c9c4614dc1aedc40789b05348d14c11d5b104e8a
deeksha-r-a <deeksha.251ec119@nitk.edu.in>
2026-05-24 15:47:58 +0530

    Create 04_Adder.v

---
+++ assignment1/hdlbits_sols/combinational_logic/arithematic_circuits/04_Adder.v
@@ -0,0 +1,14 @@
+module top_module (
+    input [3:0] x,
+    input [3:0] y, 
+    output [4:0] sum);
+    wire w1,w2,w3;
+    FA a1(.a(x[0]),.b(y[0]),.cin(1'b0),.sum(sum[0]),.cout(w1));
+    FA a2(.a(x[1]),.b(y[1]),.cin(w1),.sum(sum[1]),.cout(w2));
+    FA a3(.a(x[2]),.b(y[2]),.cin(w2),.sum(sum[2]),.cout(w3));
+    FA a4(.a(x[3]),.b(y[3]),.cin(w3),.sum(sum[3]),.cout(sum[4]));
+endmodule
+module FA(input a,input b, input cin, output sum, output cout);
+    assign sum=a^b^cin;
+    assign cout=a&b|a&cin|b&cin;
+endmodule
