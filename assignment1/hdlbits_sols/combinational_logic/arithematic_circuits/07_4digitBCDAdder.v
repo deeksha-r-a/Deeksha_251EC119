c956018ab348cb7e6cf5ecb8aee1a59b8f404ff4
deeksha-r-a <deeksha.251ec119@nitk.edu.in>
2026-05-24 17:19:41 +0530

    Update 07_4digitBCDAdder.v

--- assignment1/hdlbits_sols/combinational_logic/arithematic_circuits/07_4digitBCDAdder.v
+++ assignment1/hdlbits_sols/combinational_logic/arithematic_circuits/07_4digitBCDAdder.v
@@ -1,1 +1,14 @@
+module top_module ( 
+    input [15:0] a, b,
+    input cin,
+    output cout,
+    output [15:0] sum );
+    wire[3:0] c1, c2, c3;
+    bcd_fadd FA0(.a(a[3:0]),.b(b[3:0]),.cin(cin),.sum(sum[3:0]),.cout(c1));
+    bcd_fadd FA1(.a(a[7:4]),.b(b[7:4]),.cin(c1),.sum(sum[7:4]),.cout(c2));
+    bcd_fadd FA2(.a(a[11:8]),.b(b[11:8]),.cin(c2),.sum(sum[11:8]),.cout(c3));
+    bcd_fadd FA3(.a(a[15:12]),.b(b[15:12]),.cin(c3),.sum(sum[15:12]),.cout(cout));
+    
+
+endmodule
 
